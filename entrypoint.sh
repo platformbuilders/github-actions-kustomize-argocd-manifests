d9e3601835e61fdf9968b54e330172c860d58782
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2021-05-25 18:59:54 -0300

    Kustomize step changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,22 @@
-#!/bin/bash
-
-#gitops-repo-name="$1"
-#gitops-repo-url="$2"
+#!/bin/sh -l
           
 if [[ "$GITOPS_BRANCH" == "develop" ]]; then
-    printf "============> Cloning $1 - Branch: $GITOPS_BRANCH"
+    printf "============> Cloning $1 - Branch: $GITOPS_BRANCH \n"
     GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
     git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
     cd $1
     
-    printf "============> Git config step"
+    printf "============> Git config step \n"
     git config --local user.email "action@github.com"
     git config --local user.name "GitHub Action"
 
-    printf "============> Develop Kustomize step"
+    printf "============> Develop Kustomize step \n"
     cd k8s/$5/overlays/dev
     sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
     kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
     cat kustomization.yaml
 
-    printf "============> Homolog Kustomize step"
+    printf "============> Homolog Kustomize step \n"
     git checkout release
     sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
     kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
