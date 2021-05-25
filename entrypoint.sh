4cc6ea34c68607f87bd11f5f88d20e6ca878bfb1
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2021-05-25 19:22:31 -0300

    Kustomize step changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,33 @@
 #!/bin/sh -l
           
 if [[ "$GITOPS_BRANCH" == "develop" ]]; then
-    printf "============> Cloning $1 - Branch: $GITOPS_BRANCH \n"
+    printf "\033[0;32m============> Cloning $1 - Branch: $GITOPS_BRANCH \033[0m\n"
     GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
     git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
     cd $1
-    
-    printf "============> Git config step \n"
     git config --local user.email "action@github.com"
     git config --local user.name "GitHub Action"
+    echo "Repo $1 cloned!!!"
 
-    printf "============> Develop Kustomize step \n"
+    printf "\033[0;32m============> Develop Kustomize step \033[0m\n"
     cd k8s/$5/overlays/dev
     sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
     kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
     cat kustomization.yaml
 
-    printf "============> Git push: Branch develop \n"
+    printf "\033[0;32m============> Git push: Branch develop \033[0m\n"
     cd ../..
     git commit -am "$6 has Built: $7 - $RELEASE_VERSION"
     git push 
 
-    printf "============> Homolog Kustomize step \n"
+    printf "\033[0;32m============> Homolog Kustomize step \033[0m\n"
     cd overlays/dev
     git checkout release
     sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
     kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
     cat kustomization.yaml
 
-    printf "============> Git push: Branch release \n"
+    printf "\033[0;32m============> Git push: Branch release \033[0m\n"
     cd ../..
     git commit -am "$6 has Built: $7 - $RELEASE_VERSION"
     git push 
