7df4bbbcc24f1b69039a9dba204b3b78dacaa2ea
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2021-05-25 17:52:01 -0300

    Kustomize step changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/sh -l
 
-gitops-repo-name=$1
-gitops-repo-url=$2
-
-
-printf "============> Git config step"
-git config --local user.email "action@github.com"
-git config --local user.name "GitHub Action"
+gitops-repo-name="$1"
+gitops-repo-url="$2"
           
 if [[ "$BRANCH" == "develop" ]]; then
     printf "============> Cloning $gitops-repo-name - Branch: $BRANCH"
-    git clone https://${{ secrets.GH_ACCESS_TOKEN }}:x-oauth-basic@$gitops-repo-url -b $BRANCH
+    git clone https://${{ secrets.GH_ACCESS_TOKEN }}:x-oauth-basic@$gitops-repo-url -b $BRANCH    
+    cd $gitops-repo-name
     
+    printf "============> Git config step"
+    git config --local user.email "action@github.com"
+    git config --local user.name "GitHub Action"
+
     printf "============> Develop Kustomize step"
-    cd $gitops-repo-name/k8s/${{ secrets.APP_ID }}/overlays/dev
+    cd k8s/${{ secrets.APP_ID }}/overlays/dev
     sed -i "s/version:.*/version: ${{ env.RELEASE_VERSION }}/g" datadog-env-patch.yaml
     kustomize edit set image IMAGE=gcr.io/${{ secrets.GCP_PROJECT_ID_PROD }}/${{ secrets.APP_ID }}:${{ env.RELEASE_VERSION }}
     cat kustomization.yaml
-    
+
     printf "============> Homolog Kustomize step"
     git checkout release
     sed -i "s/version:.*/version: ${{ env.RELEASE_VERSION }}/g" datadog-env-patch.yaml
