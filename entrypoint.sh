b9b509b9818b7eab9e39e05a01b83ef3cf4d1bc6
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2021-05-26 11:19:28 -0300

    Kustomize step changes - HML and PRD overlays

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,8 @@ if [[ "$GITOPS_BRANCH" == "develop" ]]; then
     git config --local user.name "GitHub Action"
     echo "Repo $1 cloned!!!"
 
-    printf "\033[0;32m============> Develop Kustomize step \033[0m\n"
+    ############################################################################################## Develop Kustomize - DEV Overlays
+    printf "\033[0;32m============> Develop branch Kustomize step - DEV Overlay \033[0m\n"
     cd k8s/$5/overlays/dev
     sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
     kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
@@ -17,10 +18,11 @@ if [[ "$GITOPS_BRANCH" == "develop" ]]; then
 
     printf "\033[0;32m============> Git push: Branch develop \033[0m\n"
     cd ../..
-    git commit -am "$6 has Built: $7 - $RELEASE_VERSION"
+    git commit -am "$6 has Built a new version: - $RELEASE_VERSION"
     git push 
 
-    printf "\033[0;32m============> Homolog Kustomize step \033[0m\n"
+    ############################################################################################## Release Kustomize - DEV Overlays
+    printf "\033[0;32m============> Release branch Kustomize step - DEV Overlay \033[0m\n"
     cd overlays/dev
     git checkout release
     sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
@@ -29,16 +31,46 @@ if [[ "$GITOPS_BRANCH" == "develop" ]]; then
 
     printf "\033[0;32m============> Git push: Branch release \033[0m\n"
     cd ../..
-    git commit -am "$6 has Built: $7 - $RELEASE_VERSION"
+    git commit -am "$6 has Built a new version: - $RELEASE_VERSION"
     git push 
     
-# elif [[ "$BRANCH" == "develop" ]]; then
-#     cd $1/k8s$5/overlays/homolog
-#     sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
-#     kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
+elif [[ "$GITOPS_BRANCH" == "release" ]]; then    
+    printf "\033[0;32m============> Cloning $1 - Branch: $GITOPS_BRANCH \033[0m\n"
+    GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
+    git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
+    cd $1
+    git config --local user.email "action@github.com"
+    git config --local user.name "GitHub Action"
+    echo "Repo $1 cloned!!!"
 
-#     cd ..
-#     cd prod
-#     sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
-#     kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
+    ############################################################################################## Release Kustomize - HML and PRD Overlays
+    printf "\033[0;32m============> Release branch Kustomize step - HML Overlay \033[0m\n"
+    cd k8s/$5/overlays/hml
+    sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
+    kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
+    echo "Done!!"
+
+    printf "\033[0;32m============> Release branch Kustomize step - PRD Overlay \033[0m\n"
+    cd ../prd
+    sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
+    kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
+    echo "Done!!"
+
+    printf "\033[0;32m============> Git push: Branch release \033[0m\n"
+    cd ../..
+    git commit -am "$6 has Built a new version: - $RELEASE_VERSION"
+    git push
+
+    ############################################################################################## Develop Kustomize - HML Overlays
+    printf "\033[0;32m============> Develop branch Kustomize step - HML Overlay \033[0m\n"
+    cd overlays/hml
+    git checkout develop
+    sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
+    kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
+    echo "Done!!"
+
+    printf "\033[0;32m============> Git push: Branch develop \033[0m\n"
+    cd ../..
+    git commit -am "$6 has Built a new version: - $RELEASE_VERSION"
+    git push
 fi
