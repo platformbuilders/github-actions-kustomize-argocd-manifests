e8f899d1f88606ca541f8f734bbad0c350c05a38
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2021-05-27 17:04:55 -0300

    entrypoint.sh changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
           
 if [[ "$GITOPS_BRANCH" == "develop" ]]; then
-    printf "\033[0;32m============> Cloning $1 - Branch: $GITOPS_BRANCH \033[0m\n"
-    GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
-    git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
-    cd $1
-    git config --local user.email "action@github.com"
-    git config --local user.name "GitHub Action"
-    echo "Repo $1 cloned!!!"
+    #printf "\033[0;32m============> Cloning $1 - Branch: $GITOPS_BRANCH \033[0m\n"
+    #GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
+    #git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
+    #cd $1
+    #git config --local user.email "action@github.com"
+    #git config --local user.name "GitHub Action"
+    #echo "Repo $1 cloned!!!"
 
     ############################################################################################## Develop Kustomize - DEV Overlays
     printf "\033[0;32m============> Develop branch Kustomize step - DEV Overlay \033[0m\n"
@@ -16,32 +16,32 @@ if [[ "$GITOPS_BRANCH" == "develop" ]]; then
     kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
     echo "Done!!"
 
-    printf "\033[0;32m============> Git push: Branch develop \033[0m\n"
-    cd ../..
-    git commit -am "$6 has Built a new version: $RELEASE_VERSION"
-    git push 
+    #printf "\033[0;32m============> Git push: Branch develop \033[0m\n"
+    #cd ../..
+    #git commit -am "$6 has Built a new version: $RELEASE_VERSION"
+    #git push 
 
     ############################################################################################## Release Kustomize - DEV Overlays
-    printf "\033[0;32m============> Release branch Kustomize step - DEV Overlay \033[0m\n"
-    cd overlays/dev
-    git checkout release
-    sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
-    kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
-    echo "Done!!"
+    # printf "\033[0;32m============> Release branch Kustomize step - DEV Overlay \033[0m\n"
+    # cd overlays/dev
+    # git checkout release
+    # sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
+    # kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
+    # echo "Done!!"
 
-    printf "\033[0;32m============> Git push: Branch release \033[0m\n"
-    cd ../..
-    git commit -am "$6 has Built a new version: $RELEASE_VERSION"
-    git push 
+    # printf "\033[0;32m============> Git push: Branch release \033[0m\n"
+    # cd ../..
+    # git commit -am "$6 has Built a new version: $RELEASE_VERSION"
+    # git push 
     
 elif [[ "$GITOPS_BRANCH" == "release" ]]; then    
-    printf "\033[0;32m============> Cloning $1 - Branch: $GITOPS_BRANCH \033[0m\n"
-    GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
-    git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
-    cd $1
-    git config --local user.email "action@github.com"
-    git config --local user.name "GitHub Action"
-    echo "Repo $1 cloned!!!"
+    # printf "\033[0;32m============> Cloning $1 - Branch: $GITOPS_BRANCH \033[0m\n"
+    # GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
+    # git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
+    # cd $1
+    # git config --local user.email "action@github.com"
+    # git config --local user.name "GitHub Action"
+    # echo "Repo $1 cloned!!!"
 
     ############################################################################################## Release Kustomize - HML and PRD Overlays
     printf "\033[0;32m============> Release branch Kustomize step - HML Overlay \033[0m\n"
@@ -56,10 +56,10 @@ elif [[ "$GITOPS_BRANCH" == "release" ]]; then
     kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
     echo "Done!!"
 
-    printf "\033[0;32m============> Git push: Branch release \033[0m\n"
-    cd ../..
-    git commit -am "$6 has Built a new version: $RELEASE_VERSION"
-    git push
+    # printf "\033[0;32m============> Git push: Branch release \033[0m\n"
+    # cd ../..
+    # git commit -am "$6 has Built a new version: $RELEASE_VERSION"
+    # git push
 
     ############################################################################################## Develop Kustomize - HML Overlays
     # printf "\033[0;32m============> Develop branch Kustomize step - HML Overlay \033[0m\n"
