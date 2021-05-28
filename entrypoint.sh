eb0e79706e60bb404b70565d7e53f1c0714d03b6
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2021-05-28 10:50:54 -0300

    entrypoint.sh changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,8 +39,10 @@ elif [[ "$GITOPS_BRANCH" == "release" ]]; then
     # GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
     # git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
     # cd $1
-    # git config --local user.email "action@github.com"
-    # git config --local user.name "GitHub Action"
+    git config --local user.email "action@github.com"
+    git config --local user.name "GitHub Action"
+    git remote set-url upstream https://$3:x-oauth-basic@$2
+    git remote set-url origin https://$3:x-oauth-basic@$2
     # echo "Repo $1 cloned!!!"
 
     ############################################################################################## Release Kustomize - HML and PRD Overlays
