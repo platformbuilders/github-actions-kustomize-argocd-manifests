f8e2cd07bd2beba860056eddeec1a1f2241b1c46
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2021-05-28 12:15:39 -0300

    entrypoint.sh changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
           
 if [[ "$GITOPS_BRANCH" == "develop" ]]; then
-    #printf "\033[0;32m============> Cloning $1 - Branch: $GITOPS_BRANCH \033[0m\n"
-    #GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
-    #git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
-    #cd $1
-    #git config --local user.email "action@github.com"
-    #git config --local user.name "GitHub Action"
-    #echo "Repo $1 cloned!!!"
+    printf "\033[0;32m============> Cloning $1 - Branch: $GITOPS_BRANCH \033[0m\n"
+    GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
+    git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
+    cd $1
+    git config --local user.email "action@github.com"
+    git config --local user.name "GitHub Action"
+    echo "Repo $1 cloned!!!"
 
     ############################################################################################## Develop Kustomize - DEV Overlays
     printf "\033[0;32m============> Develop branch Kustomize step - DEV Overlay \033[0m\n"
@@ -16,36 +16,32 @@ if [[ "$GITOPS_BRANCH" == "develop" ]]; then
     kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
     echo "Done!!"
 
-    #printf "\033[0;32m============> Git push: Branch develop \033[0m\n"
-    #cd ../..
-    #git commit -am "$6 has Built a new version: $RELEASE_VERSION"
-    #git push 
+    printf "\033[0;32m============> Git push: Branch develop \033[0m\n"
+    cd ../..
+    git commit -am "$6 has Built a new version: $RELEASE_VERSION"
+    git push 
 
     ############################################################################################## Release Kustomize - DEV Overlays
-    # printf "\033[0;32m============> Release branch Kustomize step - DEV Overlay \033[0m\n"
-    # cd overlays/dev
-    # git checkout release
-    # sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
-    # kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
-    # echo "Done!!"
+    printf "\033[0;32m============> Release branch Kustomize step - DEV Overlay \033[0m\n"
+    cd overlays/dev
+    git checkout release
+    sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
+    kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
+    echo "Done!!"
 
-    # printf "\033[0;32m============> Git push: Branch release \033[0m\n"
-    # cd ../..
-    # git commit -am "$6 has Built a new version: $RELEASE_VERSION"
-    # git push 
+    printf "\033[0;32m============> Git push: Branch release \033[0m\n"
+    cd ../..
+    git commit -am "$6 has Built a new version: $RELEASE_VERSION"
+    git push 
     
 elif [[ "$GITOPS_BRANCH" == "release" ]]; then    
     printf "\033[0;32m============> Cloning $1 - Branch: $GITOPS_BRANCH \033[0m\n"
-    # GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
-    # git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
-    # cd $1
+    GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
+    git clone $GITOPS_REPO_FULL_URL -b $GITOPS_BRANCH
+    cd $1
     git config --local user.email "action@github.com"
     git config --local user.name "GitHub Action"
-    git remote set-url upstream https://$3:x-oauth-basic@$2
-    git remote set-url origin https://$3:x-oauth-basic@$2
-    git remote rename origin upstream
-    git remote rename "GitHub Action" origin
-    # echo "Repo $1 cloned!!!"
+    echo "Repo $1 cloned!!!"
 
     ############################################################################################## Release Kustomize - HML and PRD Overlays
     printf "\033[0;32m============> Release branch Kustomize step - HML Overlay \033[0m\n"
@@ -66,15 +62,18 @@ elif [[ "$GITOPS_BRANCH" == "release" ]]; then
     git push
 
     ############################################################################################## Develop Kustomize - HML Overlays
-    # printf "\033[0;32m============> Develop branch Kustomize step - HML Overlay \033[0m\n"
-    # cd overlays/homolog
-    # git checkout develop
-    # sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
-    # kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
-    # echo "Done!!"
+    printf "\033[0;32m============> Develop branch Kustomize step - HML Overlay \033[0m\n"
+    cd overlays/homolog
+    git checkout develop
+    sed -i "s/version:.*/version: $RELEASE_VERSION/g" datadog-env-patch.yaml
+    kustomize edit set image IMAGE=gcr.io/$4$5:$RELEASE_VERSION
+    echo "Done!!"
 
-    # printf "\033[0;32m============> Git push: Branch develop \033[0m\n"
-    # cd ../..
-    # git commit -am "$6 has Built a new version: $RELEASE_VERSION"
-    # git push
+    printf "\033[0;32m============> Git push: Branch develop \033[0m\n"
+    cd ../..
+    git commit -am "$6 has Built a new version: $RELEASE_VERSION"
+    git push
+
+    printf "\033[0;32m============> GOING BACK TO RELEASE BRANCH BEFORE PR!!! \033[0m\n"
+    git checkout release
 fi
