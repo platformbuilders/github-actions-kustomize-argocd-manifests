199468aa5679215ba7d470d55a81efe8146ad9c1
Renato Ruis <renatoruis@pop-os.localdomain>
2022-04-27 10:33:01 -0300

    ajuste atma

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ elif [[ "$GITOPS_BRANCH" == "homolog" ]]; then
     printf "\033[0;36m================================================================================================================> Condition 2: Homolog environment \033[0m\n"
     printf "\033[0;32m============> Cloning $1 - Branch: release \033[0m\n"
     GITOPS_REPO_FULL_URL="https://$3:x-oauth-basic@$2"
-    git clone $GITOPS_REPO_FULL_URL -b develop
+    git clone $GITOPS_REPO_FULL_URL -b release
     cd $1
     git config --local user.email "action@github.com"
     git config --local user.name "GitHub Action"
@@ -43,12 +43,10 @@ elif [[ "$GITOPS_BRANCH" == "homolog" ]]; then
     printf "\033[0;32m============> Git commit and push \033[0m\n"
     cd ../..
     git commit -am "$6 has Built a new version: $RELEASE_VERSION"
-    git push origin develop
-
-    printf "\033[0;32m============> Merge develop in to release branch \033[0m\n"
-    git checkout release
-    git merge develop
     git push origin release
+    git checkout homolog
+    git merge release
+    git push origin homolog
 
 elif [[ "$GITOPS_BRANCH" == "main" ]]; then
     printf "\033[0;36m================================================================================================================> Condition 3: New release PRD environment \033[0m\n"
